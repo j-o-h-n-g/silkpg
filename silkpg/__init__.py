adff8eb457a4cfd5cc45bf53c9408913573619bd
john <john@fractal.local>
2014-12-31 12:04:44 +0000

    Refactored "contains"

--- silkpg/__init__.py
+++ silkpg/__init__.py
@@ -5,6 +5,7 @@ import datetime
 import silk
 import sys
 
+
 class SilkDataWrapper(ForeignDataWrapper):
 
     def __init__(self, options, columns):
@@ -14,10 +15,14 @@ class SilkDataWrapper(ForeignDataWrapper):
 	silk.site.init_site(siteconf=options['siteconf'], rootdir=options['rootdir'])
 
     def execute(self, quals, columns):
-	operators={'=': 'eq', '<':'lt', '>':'gt', '<=': 'le', '>=': 'ge', '!=': 'ne', '<<=': 'contains'}
+	# Dict of postgres operators to python functions
+	operators={'=': 'eq', '<':'lt', '>':'gt', '<=': 'le', '>=': 'ge', '!=': 'ne', '<<=': 'within', '>>=': 'contains'}
+	# Define our own reversed "contains"
+	operator.within=lambda a,b: a in b
+
 	stime=datetime.datetime(2000,1,1)
 	etime=datetime.datetime(2032,1,1)
-        sensors=None
+        sensor=None
 
 	if self.debug:
 		utils.log_to_postgres((quals),logging.INFO)
@@ -28,33 +33,29 @@ class SilkDataWrapper(ForeignDataWrapper):
 			stime=qual.value
 		elif qual.field_name == 'etime' and qual.operator in ['<','<='] and qual.value < etime:
 			etime=qual.value
-		elif qual.field_name == 'sensors' and qual.operator == '=':
-			sensors=qual.value
+		elif qual.field_name == 'sensor' and qual.operator == '=':
+			sensor=qual.value
  
 	if self.debug:
 		utils.log_to_postgres("Searching for data between %s and %s" % (stime,etime),logging.INFO)
 
-	for file in silk.site.repository_silkfile_iter(start=stime, end=etime, sensors=sensors):
+	for file in silk.site.repository_silkfile_iter(start=stime, end=etime, sensors=sensor):
 		# Examine each record in the file
 		for rec in file:
  			# Check each qualifier (AND)
 			for qual in quals:
 				# If qualifier is an IP convert to silk Wildcard IP
-				if qual.field_name in ['sip','dip']:
+				if qual.field_name in ['nhip','sip','dip']:
 					value=silk.IPWildcard(qual.value)
 				else:
 					value=qual.value
 	
-				# contains is reversed!  If no match then check next flow
-				if operators[qual.operator] == 'contains':
-					if not getattr(operator,operators[qual.operator])(value,getattr(rec,qual.field_name)):
-						break
-				else:
-					if not getattr(operator,operators[qual.operator])(getattr(rec,qual.field_name), value):
-						break
-
-			# Return the data
-			line={}	
-			for column_name in self.columns:
-		       		line[column_name] = '%s' % (getattr(rec,column_name))
-            		yield line
+			        if not getattr(operator,operators[qual.operator])(getattr(rec,qual.field_name), value):
+					break
+			else: 
+				# All qualifiers matched
+				# Return the data
+				line={}	
+				for column_name in self.columns:
+		       			line[column_name] = '%s' % (getattr(rec,column_name))
+            			yield line
